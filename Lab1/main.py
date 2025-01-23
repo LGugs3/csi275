bdd21b1e315736be644e24402d88d83ece025eff
LGugs3 <liam.gugliotta@mymail.champlain.edu>
2025-01-23 12:34:28 -0500

    Completed Lab2 and modified Lab1 to follow flake8

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,33 +1,61 @@
+"""
+User creates a list of integers + floats which is then sorted and printed.
+
+Author: Liam Gugliotta
+Class: CSI-275-01
+Assignment: Lab 1 -- Sorting With Python
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
+
+
 def build_list():
-    exitKeyword = "exit"
-    unsortedList = []
-    valToAppend = ""
+    """
+    Docstring here.
 
-    while valToAppend != exitKeyword:
-        valToAppend = input(f"Enter Number(type {exitKeyword} to stop): ")
-        if valToAppend == exitKeyword: break
+    double lines
+    """
+    exit_keyword = "exit"
+    unsorted_list = []
+    val_to_append = ""
 
-        if(valToAppend.isdigit()): unsortedList.append(int(valToAppend))
+    while val_to_append != exit_keyword:
+        val_to_append = input(f"Enter Number(type {exit_keyword} to stop): ")
+        if val_to_append == exit_keyword:
+            break
+
+        if (val_to_append.isdigit()):
+            unsorted_list.append(int(val_to_append))
         else:
             try:
-                valToAppend = float(valToAppend)
+                val_to_append = float(val_to_append)
             except ValueError:
-                print(f"{valToAppend} is not an integer or float")
+                print(f"{val_to_append} is not an integer or float")
             except Exception as e:
-                print(type(e).__name__, ':',e,"\n\nSomething went critically wrong; How did you do this.")
+                print(type(e).__name__, ':', e, "\n\nSomething went critically\
+                       wrong; How did you do this.")
             else:
-                unsortedList.append(float(valToAppend))
-    
-    return unsortedList
+                unsorted_list.append(float(val_to_append))
+
+    return unsorted_list
 
 
-def sort_list(unsortedList):
-    unsortedList.sort()
+def sort_list(unsorted_list):
+    """Another docstring."""
+    unsorted_list.sort()
 
 
 if __name__ == "__main__":
-    myList = build_list()
-    print(myList)
-    sort_list(myList)
+    my_list = build_list()
+    print(my_list)
+    sort_list(my_list)
 
-    print(myList)
+    print(my_list)
---
+++ Lab2/hosts.py
@@ -0,0 +1,186 @@
+"""
+Verifying IPs and hostnames.
+
+Author: Liam Gugliotta
+Class: CSI-275-01
+Assignment: Lab 2 -- Host Names and IP Addresses
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+
+---------------------------------------------------------------------------------------
+
+Student code for Lab2.
+
+    Run python autograder.py
+
+Champlain College CSI-235, Spring 2019
+The following code was written by Joshua Auerbach (jauerbach@champlain.edu)
+Host class __init__ function by Jason Reeves 1/4/2021 (reeves@champlain.edu)
+"""
+from util import raise_not_defined
+import re
+
+
+class InvalidEntryError(Exception):
+    """Exception raised for invalid entries in the hosts file."""
+
+    pass
+
+
+def is_valid_ip_address(ip_address: str) -> bool:
+    """Return whether the given ip_address is a valid IPv4 address or not.
+
+    Args:
+        ip_address (str): ip_address to test
+
+    Returns:
+        bool: True if ip_address is valid IPv4 address, False otherwise.
+
+    """
+    octects = str(ip_address).split('.')
+    if len(octects) != 4:
+        return False
+    for octect in octects:
+        if not octect.isnumeric():
+            return False
+        if int(octect) < 0 or int(octect) > 255:
+            return False
+    return True
+
+    raise_not_defined()
+
+
+def is_valid_hostname(hostname: str) -> bool:
+    """Return whether the given hostname is valid or not.
+
+    Host names may contain only alphanumeric characters, minus signs ("-"),
+    and periods (".").  They must begin with an alphabetic character and end
+    with an alphanumeric character.
+
+    Args:
+        hostname (str): hostname to test
+
+    Returns:
+        bool: True if hostname is valid, False otherwise.
+
+    """
+    hostname_regex = "^[a-zA-Z]{1}[a-zA-Z0-9.-]*[a-zA-Z0-9]?$"
+    return True if not re.match(hostname_regex, hostname) is None else False
+
+    raise_not_defined()
+
+
+class Hosts:
+    """The Hosts class handles translating hostnames to ip addresses."""
+
+    def __init__(self, hosts_file):
+        """Initialize the Hosts class.
+
+        Imports all of the host names and addresses
+        from the provided hosts_file. If the file does
+        not follow the proper format or contains
+        invalid IP addresses, hostnames, or aliases,
+        an InvalidEntryError is raised.
+
+        If successful, this function fills two lists
+        (self.ips and self.hostnames) that together
+        represent IP/hostname and IP/alias mappings
+        in the parsed file. The hostname at index i
+        in self.hostnames will correspond to the IP
+        at index i in self.ips.
+
+        For example, if the first line of a hosts file
+        maps localhost to 127.0.0.1, then
+        self.hostnames[0] = 'localhost' and
+        self.ips[0] = '127.0.0.1'.
+        """
+        f = open(hosts_file, "r")
+
+        self.ips = []
+        self.hostnames = []
+
+        line = f.readline()
+        while line:
+            # If the line is a comment, skip it
+            if line[0] == "#":
+                line = f.readline()
+                continue
+
+            # If the line is blank, skip it
+            if line[0] == "\n":
+                line = f.readline()
+                continue
+
+            # The first 16 characters represent the IP address
+            ip_address = line[0:16]
+            print(f"IP = {ip_address}")
+            # Make sure to remove any trailing whitespace!
+            if not is_valid_ip_address(ip_address.rstrip()):
+                print("Bad IP")
+                raise InvalidEntryError
+
+            # The hostname is the first string
+            # starting from the 17th character,
+            # and aliases are anything after that
+            rest_of_line = line[16:].split(" ")
+            # hostname = rest_of_line[0]
+            has_hostname = False
+            for hostname in rest_of_line:
+                print(hostname.rstrip())
+                # If we see a comment, the rest of the line should be tossed
+                if hostname.rstrip() == "#":
+                    print("We're done here")
+                    break
+                if not is_valid_hostname(hostname.rstrip()):
+                    print("Bad Hostname")
+                    if hostname.rstrip() != "":
+                        raise InvalidEntryError
+                else:
+                    # If we reach here, the line is valid, so
+                    # add it to our mapping
+                    has_hostname = True
+                    self.ips.append(ip_address.rstrip())
+                    self.hostnames.append(hostname.rstrip())
+
+            # If we didn't find a hostname before,
+            # raise an error
+            if not has_hostname:
+                raise InvalidEntryError
+
+            # Read the next line
+            line = f.readline()
+
+    def contains_entry(self, hostname: str) -> bool:
+        """Return whether or not a given hostname exists."""
+        try:
+            self.hostnames.index(hostname)
+        except ValueError:
+            return False
+        except Exception as e:
+            print(f"Exception {e.__class__.__name__} occured.\n{e}")
+        else:
+            return True
+
+    def get_ip(self, hostname: str) -> str:
+        """Return the IP for a given hostname.
+
+        If the hostname does not exist in the file,
+        None is returned.
+        """
+        index = -1
+        try:
+            index = self.hostnames.index(hostname)
+        except ValueError:
+            return None
+        except Exception as e:
+            print(f"Exception {e.__class__.__name__} occured.\n{e}")
+        else:
+            return self.ips[index]
